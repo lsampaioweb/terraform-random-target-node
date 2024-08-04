9e39ea516e8acc0f06a59ba88a6265a627a865bb
Luciano Sampaio <lsampaioweb@gmail.com>
2024-08-04 10:39:19 -0300

    chore: Update the random provider version from 3.4.3 to 3.6.2.

--- examples/providers.tf
+++ examples/providers.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     random = {
+      # https://registry.terraform.io/providers/hashicorp/random/latest
       source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "3.6.2"
     }
   }
 }
