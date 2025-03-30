979e2e3a3e9badc6a36b968f490bded5b73cc1f7
Luciano Sampaio <lsampaioweb@gmail.com>
2025-03-30 18:54:13 -0300

    fix: Correct output values in output.tf for random_integer resource

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "all" {
-  value       = random_integer.target_node
   description = "All the returned values for the random_integer resource."
+  value       = random_integer.target_node
 }
 
 output "id" {
-  value       = random_integer.target_node.id
   description = "The string representation of the integer result."
+  value       = random_integer.target_node.id
 }
 
 output "result" {
-  value       = random_integer.target_node.result
   description = "The random integer result."
+  value       = random_integer.target_node.result
 }
 
 output "formatted_result" {
-  value       = join(var.separator, [var.prefix, format(var.scale, random_integer.target_node.result)])
   description = "The random integer result formmated with the node prefix and separator."
+  value       = join(var.separator, [var.prefix, format(var.scale, random_integer.target_node.result)])
 }
